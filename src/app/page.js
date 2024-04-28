493f1ad199f667556e066ff9d6d13fdafb25b9a2
itforget <itforget11@gmail.com>
2024-04-27 23:04:56 -0300

    add: reset button

--- src/app/page.js
+++ src/app/page.js
@@ -12,10 +12,20 @@ export default function Home() {
   const videoRef = useRef();
   const [handleClick, setClick] = useState(false);
 
-  if (handleClick) {
+  if (handleClick === true) {
     const video = videoRef.current;
-    video.play();
+    if (video) {
+      video.play();
+    }
+  } else {
+    const video = videoRef.current;
+    if (video) {
+      video.pause();
+      video.currentTime = 0;
+    }
   }
+
+
   return (
     <main className={`flex min-h-screen flex-col items-center p-24 ${darkMode ? "bg-gray-900 text-white" : "bg-[var(--foreground-rgb: 0, 0, 0, --background-start-rgb: 214, 219, 220 ,--background-end-rgb: 255, 255, 255)] text-black"}`}>
       <button className="absolute top-0 right-0 p-3" onClick={toggleDarkMode}>
@@ -29,11 +39,12 @@ export default function Home() {
         </a>
         <h1 className="text-3xl font-bold">Click on the Pedro</h1>
       </div>
-      <video ref={videoRef} className={handleClick ? "rounded-full" : "rounded-full hidden"} width="620" height="620" style={{ clipPath: "circle()" }}>
-          <source src="/pedro.mp4" type="video/mp4" />
+      <div className="flex items-center justify-center flex-col gap-5">
+        <video ref={videoRef} className={handleClick ? "rounded-full" : "rounded-full hidden"} width="620" height="620" style={{ clipPath: "circle()" }}>
+            <source src="/pedro.mp4" type="video/mp4" />
         </video>
+        <button className={`text-2xl font-bold ${handleClick ? "" : "hidden"}`} onClick={() => setClick(false)} type="button">Reset</button>
+      </div>
     </main>
   );
 }
-
-
